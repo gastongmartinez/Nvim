d71f466f8d870dfacc82a6489e63150cc7689edc
Gastón Martínez <gastongmartinez@gmail.com>
2021-11-07 17:58:33 -0300

    Inicial

---
+++ lua/options.lua
@@ -0,0 +1,30 @@
+-- Opciones
+vim.o.number = true
+vim.o.vb = false
+vim.o.syntax = 'on'
+vim.wo.relativenumber = true
+vim.wo.cursorline = true
+vim.bo.autoindent = true
+vim.bo.expandtab = true
+vim.bo.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.smarttab = true
+vim.bo.softtabstop = 4
+vim.o.mouse = 'a'
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.wo.wrap = false
+vim.o.ruler = true
+vim.o.clipboard = 'unnamedplus'
+vim.o.termguicolors = true
+vim.o.completeopt = 'menu,menuone,noselect'
+vim.o.splitright = true
+vim.o.splitbelow = true
+if vim.fn.has('win32') == 1 then
+  vim.o.shell='pwsh.exe'
+end
+
+-- Reconocimiento de tipo de archivo
+vim.cmd('filetype on')
+vim.cmd('filetype plugin on')
+vim.cmd('filetype indent on')
---
+++ lua/plugins.lua
@@ -0,0 +1,39 @@
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.cmd 'packadd packer.nvim'
+end
+
+vim.cmd [[packadd packer.nvim]]
+require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  --use 'glepnir/dashboard-nvim'
+  --use 'navarasu/onedark.nvim'
+  --use {'romgrk/barbar.nvim', requires = {'kyazdani42/nvim-web-devicons'}}
+  --use {'hoob3rt/lualine.nvim', requires = {'kyazdani42/nvim-web-devicons', opt = true}}
+  --use 'folke/which-key.nvim'
+  --use 'kyazdani42/nvim-tree.lua'
+  --use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+  --use 'nvim-treesitter/playground'
+  --use {'nvim-telescope/telescope.nvim', requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}}
+  --use 'norcalli/nvim-colorizer.lua'
+  --use 'lukas-reineke/indent-blankline.nvim'
+  --use 'williamboman/nvim-lsp-installer'
+  --use 'neovim/nvim-lspconfig'
+  --use 'hrsh7th/cmp-nvim-lsp'
+  --use 'hrsh7th/cmp-buffer'
+  --use 'hrsh7th/nvim-cmp'
+  --use 'hrsh7th/cmp-vsnip'
+  --use 'hrsh7th/vim-vsnip'
+  --use 'hrsh7th/cmp-nvim-lua'
+  --use 'hrsh7th/cmp-latex-symbols'
+  --use 'onsails/lspkind-nvim'
+  --use 'glepnir/lspsaga.nvim'
+  --use 'mhartington/formatter.nvim'
+  --use 'b3nj5m1n/kommentary'
+  --use 'windwp/nvim-autopairs'
+  --use { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim' }
+  --use 'sindrets/diffview.nvim'
+  --use 'lewis6991/gitsigns.nvim'
+end)
