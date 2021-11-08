064f9fcc23d6de8a59dd53dc48a11c83cf86c0a3
Gastón Martínez <gastongmartinez@gmail.com>
2021-11-07 22:08:06 -0300

    BarBar

---
+++ lua/barbar.lua
@@ -0,0 +1,14 @@
+vim.g.bufferline = {
+  animation = true,
+  auto_hide = false,
+  tabpages = true,
+  closable = true,
+  clickable = true,
+  icons = true,
+  insert_at_end = true,
+  maximum_padding = 1,
+  maximum_length = 30,
+  semantic_letters = true,
+  letters = 'asdfjkl;ghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERUTYQP',
+  no_name_title = nil,
+}
