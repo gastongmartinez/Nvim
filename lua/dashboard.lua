39e862fcac23b3070b41740deeaa153fdf488803
Gastón Martínez <gastongmartinez@gmail.com>
2021-11-07 22:08:14 -0300

    Dashboard

---
+++ lua/dashboard.lua
@@ -0,0 +1,21 @@
+vim.g.dashboard_custom_header = {
+  ' ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗',
+  ' ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║',
+  ' ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║',
+  ' ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║',
+  ' ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║',
+  ' ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝',
+ }
+
+vim.g.dashboard_custom_footer = {}
+vim.g.dashboard_default_executive = 'telescope'
+vim.g.dashboard_custom_shortcut = {
+    last_session       = 'SPC s l',
+    find_history       = 'SPC f r',
+    find_file          = 'SPC f f',
+    new_file           = 'SPC c n',
+    change_colorscheme = 'SPC t c',
+    find_word          = 'SPC f a',
+    book_marks         = 'SPC f b',
+ }
+vim.g.indentLine_fileTypeExclude = {'dashboard'}
