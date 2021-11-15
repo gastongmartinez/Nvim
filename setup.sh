1be7bdb25159dcd975ce5cc8bbda543d4bd4c210
Gastón Martínez <gastongmartinez@gmail.com>
2021-11-15 01:55:47 -0300

    Plenary

--- setup.sh
+++ setup.sh
@@ -8,4 +8,8 @@ cargo install stylua
 
 sudo npm i -g prettier bash-language-server vscode-langservers-extracted emmet-ls typescript typescript-language-server yaml-language-server
 
-
+read -rp "Corregir plenary? (S/N): " PL
+if [ "$PL" == 'S' ]; then
+    ln -sf ~/.local/share/nvim/site/pack/packer/start/plenary.nvim/lua/plenary ~/.config/nvim/lua/plenary
+    ln -sf ~/.local/share/nvim/site/pack/packer/start/plenary.nvim/lua/plenary/vararg/init.lua ~/.local/share/nvim/site/pack/packer/start/plenary.nvim/lua/plenary/vararg.lua
+fi
