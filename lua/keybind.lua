e02b76a35dd02b93eb21318676132549209a155d
Gastón Martínez <gastongmartinez@gmail.com>
2021-11-26 20:07:29 -0300

    LSP keybinds

--- lua/keybind.lua
+++ lua/keybind.lua
@@ -1,15 +1,15 @@
 local function map(mode, lhs, rhs, opts)
-  local options = {noremap = true, silent = true}
+  local options = { noremap = true, silent = true }
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
 -- General
 vim.g.mapleader = " "
-map ('i', 'jk', '<ESC>')
-map ('i', 'kj', '<ESC>')
+map("i", "jk", "<ESC>")
+map("i", "kj", "<ESC>")
 
 -- LSP Saga
-map('n', '<C-j>', ':Lspsaga diagnostic_jump_next<CR>')
-map('n', 'K', ':Lspsaga hover_doc<CR>')
-map('n', '<C-k>', ':Lspsaga signature_help<CR>')
-map('n', 'gh', ':Lspsaga lsp_finder<CR>')
+map("n", "<C-j>", ":Lspsaga diagnostic_jump_next<CR>")
+map("n", "K", ":Lspsaga hover_doc<CR>")
+map("n", "<C-k>", ":Lspsaga signature_help<CR>")
+map("n", "gh", ":Lspsaga lsp_finder<CR>")
--- lua/lspconf.lua
+++ lua/lspconf.lua
@@ -2,37 +2,43 @@ local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protoco
 local on_attach = function(_, bufnr)
   vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-  local opts = { noremap = true, silent = true }
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(
-    bufnr,
-    "n",
-    "<leader>wl",
-    "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>",
-    opts
-  )
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-  -- vim.api.nvim_buf_set_keymap(bufnr, 'v', '<leader>ca', '<cmd>lua vim.lsp.buf.range_code_action()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(
-    bufnr,
-    "n",
-    "<leader>so",
-    [[<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>]],
-    opts
-  )
+  --  local opts = { noremap = true, silent = true }
+
+  local wk = require("which-key")
+  local lspmappings = {
+    j = {
+      name = "Jump to...",
+      t = { "<cmd>lua vim.lsp.buf.type_definition()<CR>", "Type definition" },
+      D = { "<cmd>lua vim.lsp.buf.declaration()<CR>", "Declaration" },
+      d = { "<cmd>lua vim.lsp.buf.definition()<CR>", "Definition" },
+    },
+    h = { "<cmd>lua vim.lsp.buf.hover()<CR>", "Hover information" },
+    s = { "<cmd>lua vim.lsp.buf.signature_help()<CR>", "Signature help" },
+    c = { "<cmd>lua vim.lsp.buf.code_action()<CR>", "Select code action" },
+    r = { "<cmd>lua vim.lsp.buf.rename()<CR>", "Rename all references to the symbol" },
+    D = {
+      name = "Diagnostics",
+      l = { "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", "Show line diagnostics" },
+      n = { "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", "Go to next" },
+      p = { "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", "Go to previous" },
+      q = { "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", "Set location list" },
+    },
+    l = {
+      name = "List",
+      s = { "<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>", "Document symbols" },
+      i = { "<cmd>lua vim.lsp.buf.implementation()<CR>", "All implementations of the symbol" },
+      r = { "<cmd>lua vim.lsp.buf.references()<CR>", "All references of the symbol" },
+    },
+    W = {
+      name = "Workspaces",
+      a = { "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", "Add workspace folder" },
+      r = { "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", "Remove workspace folder" },
+      l = { "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", "List workspaca folders" },
+    },
+  }
+  local lspopts = { prefix = "<leader>", buffer = bufnr }
+  wk.register(lspmappings, lspopts)
+
   vim.cmd([[ command! Format execute 'lua vim.lsp.buf.formatting()' ]])
 end
 
