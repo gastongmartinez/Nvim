803dfa64a6d77dfee992fd26fd821db466cabfe4
Gastón Martínez <gastongmartinez@gmail.com>
2021-11-26 15:37:14 -0300

    Telescope

--- lua/whichkey.lua
+++ lua/whichkey.lua
@@ -26,6 +26,13 @@ local mappings = {
     l = { ":Neogit log<cr>", "Log" },
     p = { ":Neogit push<cr>", "Push" },
   },
+  t = {
+    name = "Telescope",
+    f = { ":Telescope find_files<CR>", "Find Files" },
+    g = { ":Telescope live_grep<CR>", "Live Grep" },
+    b = { ":Telescope buffers<CR>", "Buffers" },
+    h = { ":Telescope help_tags<CR>", "Help" },
+  },
 }
 local opts = { prefix = "<leader>" }
 wk.register(mappings, opts)
