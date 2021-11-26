6f1e2478399d0da8342d47fad0a9992f1336d32b
Gastón Martínez <gastongmartinez@gmail.com>
2021-11-26 16:31:25 -0300

    Limpieza

--- lua/keybind.lua
+++ lua/keybind.lua
@@ -7,20 +7,9 @@ end
 vim.g.mapleader = " "
 map ('i', 'jk', '<ESC>')
 map ('i', 'kj', '<ESC>')
---map ('n', '<Leader>ec', ':tabedit $MYVIMRC<CR>')
 
 -- LSP Saga
 map('n', '<C-j>', ':Lspsaga diagnostic_jump_next<CR>')
 map('n', 'K', ':Lspsaga hover_doc<CR>')
 map('n', '<C-k>', ':Lspsaga signature_help<CR>')
 map('n', 'gh', ':Lspsaga lsp_finder<CR>')
-
--- Dashboard
---map ('n', '<Leader>ss', ':<C-u>SessionSave<CR>')
---map ('n', '<Leader>sl', ':<C-u>SessionLoad<CR>')
---map ('n', '<Leader>fr', ':DashboardFindHistory<CR>')
---map ('n', '<Leader>ff', ':DashboardFindFile<CR>')
---map ('n', '<Leader>tc', ':DashboardChangeColorscheme<CR>')
---map ('n', '<Leader>fa', ':DashboardFindWord<CR>')
---map ('n', '<Leader>fb', ':DashboardJumpMark<CR>')
---map ('n', '<Leader>cn', ':DashboardNewFile<CR>')
