8a0efff7b0f54fb5554072a9dcf6a708939c6b59
Gastón Martínez <gastongmartinez@gmail.com>
2021-11-30 03:33:33 -0300

    Kommentary default mappings

--- lua/options.lua
+++ lua/options.lua
@@ -1,7 +1,7 @@
 -- Opciones
 vim.o.number = true
 vim.o.vb = false
-vim.o.syntax = 'on'
+vim.o.syntax = "on"
 vim.wo.relativenumber = true
 vim.wo.cursorline = true
 vim.bo.autoindent = true
@@ -10,23 +10,24 @@ vim.bo.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.smarttab = true
 vim.bo.softtabstop = 4
-vim.o.mouse = 'a'
+vim.o.mouse = "a"
 vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.wo.wrap = false
 vim.o.ruler = true
-vim.o.clipboard = 'unnamedplus'
+vim.o.clipboard = "unnamedplus"
 vim.o.termguicolors = true
-vim.o.completeopt = 'menu,menuone,noselect'
+vim.o.completeopt = "menu,menuone,noselect"
 vim.o.splitright = true
 vim.o.splitbelow = true
-if vim.fn.has('win32') == 1 then
-  vim.o.shell='pwsh.exe'
+if vim.fn.has("win32") == 1 then
+  vim.o.shell = "pwsh.exe"
 end
 
 -- Reconocimiento de tipo de archivo
-vim.cmd('filetype on')
-vim.cmd('filetype plugin on')
-vim.cmd('filetype indent on')
+vim.cmd("filetype on")
+vim.cmd("filetype plugin on")
+vim.cmd("filetype indent on")
 
-vim.g.indentLine_fileTypeExclude = {'dashboard'}
+vim.g.indentLine_fileTypeExclude = { "dashboard" }
+vim.g.kommentary_create_default_mappings = true
