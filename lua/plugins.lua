13056c7068774e3e0a87877eec7d64e97eca8a22
Gastón Martínez <gastongmartinez@gmail.com>
2021-11-30 21:55:32 -0300

    Mover barbar a setup y actualizar ruta en plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -10,7 +10,7 @@ require("packer").startup(function(use)
   use("wbthomason/packer.nvim")
   use({ "glepnir/dashboard-nvim", config = "require('dashboard')" })
   use({ "navarasu/onedark.nvim", config = "require('onedark').setup {}" })
-  use({ "romgrk/barbar.nvim", requires = { "kyazdani42/nvim-web-devicons" }, config = "require('barbar')" })
+  use({ "romgrk/barbar.nvim", requires = { "kyazdani42/nvim-web-devicons" }, config = "require('setup/barbar')" })
   use({
     "hoob3rt/lualine.nvim",
     requires = { "kyazdani42/nvim-web-devicons", opt = true },
