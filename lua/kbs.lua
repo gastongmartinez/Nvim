a1a606fe9014cdc82cfeac9acfc1f14b09ea2b3d
Gastón Martínez <gastongmartinez@gmail.com>
2021-12-14 17:37:08 -0300

    LspSaga Keybindings

--- lua/kbs.lua
+++ lua/kbs.lua
@@ -7,9 +7,3 @@ end
 vim.g.mapleader = " "
 map("i", "jk", "<ESC>")
 map("i", "kj", "<ESC>")
-
--- LSP Saga
-map("n", "<C-j>", ":Lspsaga diagnostic_jump_next<CR>")
-map("n", "K", ":Lspsaga hover_doc<CR>")
-map("n", "<C-k>", ":Lspsaga signature_help<CR>")
-map("n", "gh", ":Lspsaga lsp_finder<CR>")
