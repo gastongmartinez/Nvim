554e24c3393a5693006e2272e76faec06ecf19ca
Gastón Martínez <gastongmartinez@gmail.com>
2022-02-26 04:42:01 -0300

    Modificacion de teclas rapidas

--- lua/gm/null-ls.lua
+++ lua/gm/null-ls.lua
@@ -1,25 +1,25 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-  return
-end
-
-local formatting = null_ls.builtins.formatting
-local diagnostics = null_ls.builtins.diagnostics
-
-null_ls.setup({
-  sources = {
-    formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-    formatting.black.with({ extra_args = { "--fast" } }),
-    formatting.stylua.with({ extra_args = { "--indent-width", 2, "--indent-type", "Spaces" } }),
-    diagnostics.flake8,
-    diagnostics.eslint,
-    diagnostics.markdownlint,
-    diagnostics.shellcheck,
-    diagnostics.yamllint,
-  },
-  on_attach = function(client)
-      if client.resolved_capabilities.document_formatting then
-        vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
-      end
+  return
+end
+
+local formatting = null_ls.builtins.formatting
+local diagnostics = null_ls.builtins.diagnostics
+
+null_ls.setup({
+  sources = {
+    formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+    formatting.black.with({ extra_args = { "--fast" } }),
+    formatting.stylua.with({ extra_args = { "--indent-width", 2, "--indent-type", "Spaces" } }),
+    diagnostics.flake8,
+    diagnostics.eslint,
+    diagnostics.markdownlint,
+    diagnostics.shellcheck,
+    diagnostics.yamllint,
+  },
+  on_attach = function(client)
+      if client.resolved_capabilities.document_formatting then
+        vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+      end
   end,
 })
