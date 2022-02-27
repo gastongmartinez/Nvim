077d1774f0bb24404293b347d8f485522f9e4e03
Gastón Martínez <gastongmartinez@gmail.com>
2022-02-27 02:07:45 -0300

    Black a autopep8

--- lua/gm/null-ls.lua
+++ lua/gm/null-ls.lua
@@ -9,7 +9,7 @@ local diagnostics = null_ls.builtins.diagnostics
 null_ls.setup({
   sources = {
     formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-    formatting.black.with({ extra_args = { "--fast" } }),
+    formatting.autopep8,
     formatting.stylua.with({ extra_args = { "--indent-width", 2, "--indent-type", "Spaces" } }),
     diagnostics.flake8,
     diagnostics.eslint,
@@ -18,8 +18,8 @@ null_ls.setup({
     diagnostics.yamllint,
   },
   on_attach = function(client)
-      if client.resolved_capabilities.document_formatting then
-        vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
-      end
+    if client.resolved_capabilities.document_formatting then
+      vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+    end
   end,
 })
